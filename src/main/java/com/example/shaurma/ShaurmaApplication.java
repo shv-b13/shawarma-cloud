1d928cec2f15ad9b0c7255eeefe31345b76a19e5
Bektur Shakeev <shakeev2000@gmail.com>
2024-07-11 17:25:49 +0600

    start

---
+++ src/main/java/com/example/shaurma/ShaurmaApplication.java
@@ -0,0 +1,13 @@
+package com.example.shaurma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShaurmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShaurmaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/shaurma/ShaurmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shaurma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShaurmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
